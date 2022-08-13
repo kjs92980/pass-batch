3117bd482cd71e90d0c16a3f39787d96ed621389
like-g-su <kjs9298@naver.com>
2022-08-13 16:23:22 +0900

    first commit in project

--- README.md
+++ README.md
@@ -3,6 +3,11 @@
 PT 이용권 관리 서비스 내 배치 repository 입니다.
 이용권 만료, 일괄 지급, 수업 전 알림, 수업 후 이용권 차감 기능을 제공합니다.
 
+## Environments
+* OpenJDK 18.0.1
+* Spring Boot 2.7.2
+* MySQL
+
 ## Process
 ### 이용권 만료
 ```mermaid
---
+++ src/test/java/com/zisu/pass/PtPassBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zisu.pass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtPassBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
