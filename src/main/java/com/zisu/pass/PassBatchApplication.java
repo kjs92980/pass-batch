a2f98047e2fda230ade92cfdb81ef58ae1e6ebdd
like-g-su <kjs9298@naver.com>
2022-08-13 16:56:01 +0900

    Rename main

--- src/main/java/com/zisu/pass/PassBatchApplication.java
+++ src/main/java/com/zisu/pass/PassBatchApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PtPassBatchApplication {
+public class PassBatchApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PtPassBatchApplication.class, args);
+		SpringApplication.run(PassBatchApplication.class, args);
 	}
 
 }
