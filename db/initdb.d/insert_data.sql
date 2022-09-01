ff99aa08c3b781e51d2b15e9d58ad0fb0d16fdb4
kjs92980 <kjs9298@naver.com>
2022-09-01 22:38:59 +0900

    Update db configs and initial query

--- db/initdb.d/insert_data.sql
+++ db/initdb.d/insert_data.sql
@@ -1,2 +1,18 @@
-INSERT INTO pass_local.pass (gym_id, pass_id, user_id, status, num, started_at, ended_at, expired_at, created_at, deleted) VALUES
-('GYM', 'PASS_01', '10000', 'ACTIVE', 100, '2022-08-15 00:00:00', '2022-08-16 00:00:00', NULL, '2022-08-15 00:00:00', 0);
+INSERT INTO package (package_name, count, period, created_at)
+VALUES ('Starter PT 10회', 10, 60, '2022-08-01 00:00:00'),
+       ('Starter PT 20회', 20, 120, '2022-08-01 00:00:00'),
+       ('Starter PT 30회', 30, 180, '2022-08-01 00:00:00'),
+       ('무료 이벤트 필라테스 1회', 1, NULL, '2022-08-01 00:00:00'),
+       ('바디 챌린지 PT 4주', NULL, 28, '2022-08-01 00:00:00'),
+       ('바디 챌린지 PT 8주', NULL, 48, '2022-08-01 00:00:00'),
+       ('인바디 상담', NULL, NULL, '2022-08-01 00:00:00');
+
+INSERT INTO `user` (user_id, user_name, status, phone, meta, created_at)
+VALUES ('A1000000', '우영우', 'ACTIVE', '01011112222', NULL, '2022-08-01 00:00:00'),
+       ('A1000001', '최수연', 'ACTIVE', '01033334444', NULL, '2022-08-01 00:00:00'),
+       ('A1000002', '이준호', 'INACTIVE', '01055556666', NULL, '2022-08-01 00:00:00'),
+       ('B1000010', '권민우', 'ACTIVE', '01077778888', NULL, '2022-08-01 00:00:00'),
+       ('B1000011', '동그라미', 'INACTIVE', '01088889999', NULL, '2022-08-01 00:00:00'),
+       ('B2000000', '한선영', 'ACTIVE', '01099990000', NULL, '2022-08-01 00:00:00'),
+       ('B2000001', '태수미', 'ACTIVE', '01000001111', NULL, '2022-08-01 00:00:00');
+
