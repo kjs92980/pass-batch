e550889b746353b4c7cb7247fa37822ca7d7d2a8
kjs92980 <kjs9298@naver.com>
2022-09-08 09:10:36 +0900

    Add comments to configurations

--- src/main/java/com/fastcampus/pass/config/BatchConfig.java
+++ src/main/java/com/fastcampus/pass/config/BatchConfig.java
@@ -6,7 +6,8 @@ import org.springframework.context.annotation.Configuration;
 /*
  * @EnableBatchProcessing
  * Spring Batch 기능을 활성화하고 배치 작업을 설정하기 위한 기본 구성을 제공합니다.
- * JobBuilderFactory, StepBuilderFactory를 빈으로 등록되어 Job, Step 구현 시 사용할 수 있습니다.
+ * JobRepository, JobLauncher, JobRegistry, PlatformTransactionManager, JobBuilderFactory, StepBuilderFactory 빈으로 제공됩니다.
+ * https://docs.spring.io/spring-batch/docs/current/api/org/springframework/batch/core/configuration/annotation/EnableBatchProcessing.html
  */
 
 @EnableBatchProcessing
--- src/main/java/com/fastcampus/pass/config/JpaConfig.java
+++ src/main/java/com/fastcampus/pass/config/JpaConfig.java
@@ -3,7 +3,9 @@ package com.fastcampus.pass.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-
+/**
+ * JPA(Java Persistence API): ORM(Object-Relational Mapping)영역에서 표준화된 접근법을 제공합니다.
+ */
 @EnableJpaAuditing // JPA auditing을 활성화합니다. 본 프로젝트에서는 entity의 생성일시와 수정일시를 자동화하는 용도로 사용합니다.
 @Configuration
 public class JpaConfig {
