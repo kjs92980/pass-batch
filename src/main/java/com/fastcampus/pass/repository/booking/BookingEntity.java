b6cae12ad8cbf46de7d150df9c6c83111d575b8e
kjs92980 <kjs9298@naver.com>
2022-09-19 06:58:14 +0900

    Add UsePassesJob
    - AsyncItemProcessor, AsyncItemWriter

--- src/main/java/com/fastcampus/pass/repository/booking/BookingEntity.java
+++ src/main/java/com/fastcampus/pass/repository/booking/BookingEntity.java
@@ -1,6 +1,7 @@
 package com.fastcampus.pass.repository.booking;
 
 import com.fastcampus.pass.repository.BaseEntity;
+import com.fastcampus.pass.repository.pass.PassEntity;
 import com.fastcampus.pass.repository.user.UserEntity;
 import lombok.Getter;
 import lombok.Setter;
@@ -33,5 +34,9 @@ public class BookingEntity extends BaseEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "userId", insertable = false, updatable = false)
     private UserEntity userEntity;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "passSeq", insertable = false, updatable = false)
+    private PassEntity passEntity;
 
 }
--- src/main/java/com/fastcampus/pass/repository/pass/PassRepository.java
+++ src/main/java/com/fastcampus/pass/repository/pass/PassRepository.java
@@ -1,7 +1,17 @@
 package com.fastcampus.pass.repository.pass;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import javax.transaction.Transactional;
 
 public interface PassRepository extends JpaRepository<PassEntity, Integer> {
-
+    @Transactional
+    @Modifying
+    @Query(value = "UPDATE PassEntity p" +
+            "          SET p.remainingCount = :remainingCount," +
+            "              p.modifiedAt = CURRENT_TIMESTAMP" +
+            "        WHERE p.passSeq = :passSeq")
+    int updateRemainingCount(Integer passSeq, Integer remainingCount);
 }
